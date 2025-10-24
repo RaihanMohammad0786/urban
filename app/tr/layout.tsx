0d71d68894fdcad1d04387e29486d474ef4c8e65
RaihanMohammad0786 <65467362+RaihanMohammad0786@users.noreply.github.com>
2025-10-25 03:45:11 +1100

    Update layout.tsx

--- app/tr/layout.tsx
+++ app/tr/layout.tsx
@@ -15,7 +15,7 @@ export default async function LocaleLayout({children}: {children: React.ReactNod
   return (
     <html lang='tr' dir='ltr'>
       <body>
-        <NextIntlClientProvider locale='tr' messages={...messages}>
+        <NextIntlClientProvider locale='ar' messages={messages}>
           <Nav/>
           {children}
           <Footer/>
