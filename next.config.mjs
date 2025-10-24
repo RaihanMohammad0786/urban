a39b33e70105ef87a9c301ef178d067acd5b363a
RaihanMohammad0786 <65467362+RaihanMohammad0786@users.noreply.github.com>
2025-10-25 03:31:14 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { experimental:{ serverActions:{ allowedOrigins:['*'] } } };
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: { serverActions: { allowedOrigins: ['*'] } },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
+};
+export default nextConfig;
